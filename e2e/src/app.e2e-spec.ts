f1299863c79299178ec926438bb781cfef3f8265
Juan Francisco Bernal <jota.efebr@hotmail.com>
2019-08-15 13:53:27 +0200

    Fixed Basic e2e Test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to validator-test!');
+    expect(page.getTitleText()).toEqual('Angular Date Validators');
   });
 
   afterEach(async () => {

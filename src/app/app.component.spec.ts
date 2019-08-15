e3646d026390d776d43ca8a3ef4c8bed4363b108
Juan Francisco Bernal <jota.efebr@hotmail.com>
2019-08-15 13:47:16 +0200

    Fixed Basic Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,6 +8,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        ReactiveFormsModule
+      ]
     }).compileComponents();
   }));
 
@@ -26,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to validator-test!');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular Date Validators');
   });
 });
